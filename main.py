276fc45d642334726994c40ae489f3860bff5430
0x00-pl <0x00.pl@gmail.com>
2017-07-05 00:59:59 +0800

    add filter with ping test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import json
 import subprocess
@@ -10,10 +11,24 @@ def start_sslocal(server_addr, server_port, password, local_port):
                     '-l', local_port
     ])
 
+def ping_test(addr):
+    try:
+        out = subprocess.check_output(
+            ['ping', '-c', '10', '-i', '0.21', addr]
+        ).decode('utf8')
+        total_time_r = re.search('packet loss, time (\d+)ms', out)
+        total_time = int(total_time_r.group(1))
+        print('[info]: ping test:', addr, total_time)
+        return total_time < 2200
+    except:
+        return False
+
 def handle_json(j):
     base_port = 1081
     ret = []
     for conf in j["configs"]:
+        if not ping_test(conf["server"]):
+            continue
         p = start_sslocal(conf["server"],
                       conf["server_port"],
                       conf["password"],
